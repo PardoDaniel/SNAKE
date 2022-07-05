c4c84b56b0ac8f125d9756d09c68945d74e71f64
Daniel Pardo <pardaniel17@gmail.com>
2022-07-05 12:28:53 -0500

    keep highscore

--- scoreboard.py
+++ scoreboard.py
@@ -2,11 +2,13 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 22, "normal")
 
-class Scoreboard(Turtle):
 
+class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -14,13 +16,20 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}   Highscore: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode= "w") as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
